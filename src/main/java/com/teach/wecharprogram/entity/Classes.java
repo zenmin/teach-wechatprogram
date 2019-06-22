5869313aa0df6230034d55fc16e561a9fda666d3
min.zeng <min.zen@qq.com>
2019-06-22 18:40:47 +0800

    fix bug

--- src/main/java/com/teach/wecharprogram/entity/Classes.java
+++ src/main/java/com/teach/wecharprogram/entity/Classes.java
@@ -3,7 +3,9 @@ package com.teach.wecharprogram.entity;
 import com.teach.wecharprogram.entity.base.EntityModel;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import com.baomidou.mybatisplus.annotation.TableField;
@@ -11,15 +13,16 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 /**
-* Create by Code Generator
-* JPA只用来正向生成数据库表和字段 如果不需要此字段更新 请加上注解@TableField(exist = false)和@Transient
-* @Author ZengMin
-* @Date 2019-06-15 17:59:22
-*/
+ * Create by Code Generator
+ * JPA只用来正向生成数据库表和字段 如果不需要此字段更新 请加上注解@TableField(exist = false)和@Transient
+ *
+ * @Author ZengMin
+ * @Date 2019-06-15 17:59:22
+ */
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-@ApiModel(value="")
+@ApiModel(value = "班级管理")
 @Table(name = "classes")
 @Entity
 @AllArgsConstructor
@@ -35,7 +38,7 @@ public class Classes extends EntityModel {
     private String name;
 
     @ApiModelProperty(value = "描述")
-    private String describe;
+    private String description;
 
     @ApiModelProperty(value = "学校名称  冗余")
     private String schoolName;
@@ -45,6 +48,5 @@ public class Classes extends EntityModel {
 
     @ApiModelProperty(value = "状态1启用 0禁用")
     private Integer status;
-
 
 }
