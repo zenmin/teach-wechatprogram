60003831f961958398becefb5a5ace8c86c1d52c
min.zeng <min.zen@qq.com>
2019-06-29 12:18:53 +0800

    fix bug

--- src/main/java/com/teach/wecharprogram/WechatProgramApplication.java
+++ src/main/java/com/teach/wecharprogram/WechatProgramApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * @author zm
@@ -14,7 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableScheduling
 @EnableSwagger2Doc
 @EnableAsync
-@EnableWebSecurity
 public class WechatProgramApplication {
 
     public static void main(String[] args) {
